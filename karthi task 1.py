fc4ab2bf046b7de0541afc0d046dec8b59dfa14d
jerryjerry81323-max <jerryjerry81323@gmail.com>
2026-02-10 16:23:20 +0530

    Add files via upload

---
+++ karthi task 1.py
@@ -0,0 +1,115 @@
+'''a=int(input("ENTER A VALUE :"))
+b=int(input("ENTER B VALUE :"))
+print("addition:",a+b)
+print("subtraction:",a-b)
+print("mulplication:",a*b)
+print("division:",a/b)
+print("modulus:",a%b)
+print("floor divisionb:",a//b)
+print("exponentiation:",a**b)'''
+
+
+
+
+
+'''w=int(input("ENTER VALUE:"))
+l=int(input("ENTER VALUE:"))
+z=w*l
+print("Area of rectangle:",z)'''
+
+
+
+'''s=int(input("ENTER VALUE:"))
+a=s*s
+print("Area of square:",a)'''
+
+
+
+'''
+D=int(input("ENTER VALUE :"))
+E=3.14, D*D
+print("Area of circle :",E)'''
+
+
+'''a=int(input("ENTER THE MARKS:"))
+b=int(input("ENTER THE MARKS:"))
+c=int(input("ENTER THE MARKS:"))
+d=a+b+c/3
+print("average marks",d)'''
+
+
+'''a=int(input("ENTER VALUE:"))
+b=int(input("ENTER VALUE:"))
+c=a==b
+print("Two numbers are equal:",c)'''
+
+
+'''x=int(input("ENTER VALUE:"))
+y=int(input("ENTER VALUE:"))
+z=x>y
+print("One number is greater then:",z)'''
+
+
+'''r=int(input("ENTER VALUE:"))
+s=int(input("ENTER VALUE:"))
+t=r<=s
+print("A number is less then or equal:",t)'''
+
+
+
+
+'''a=int(input("ENTER VALUE:"))
+b=a*a
+print("Square root of the number:",b)'''
+
+
+
+'''s=int(input("ENTER VALUE:"))
+t=s*s*s
+print("Cube root of the number:",t)'''
+
+'''
+a=int(input("ENTER VALUE:"))
+b=a%100
+print("Find the last 2 digit number:",b)
+'''
+
+
+
+
+x=int(input("ENTER VALUE:"))
+y=x//100
+print("Remove the last 2 digit of the number:",y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
