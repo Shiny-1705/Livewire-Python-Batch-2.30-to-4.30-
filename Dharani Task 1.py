c5ea5409ac3371ccf04a484df066a5bba4e2225b
dharineesh26 <dharanidharansuresh27@gmail.com>
2026-02-10 16:24:43 +0530

    Add files via upload

---
+++ Dharani Task 1.py
@@ -0,0 +1,53 @@
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+print("Add:",a+b)
+print("Subtract:",a-b)
+print("Multiply:",a*b)
+
+a=4
+b=5
+c=a+b
+print(c)
+
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+c=int(input("Enter c:"))
+d=/3
+print(d)
+
+
+a=2
+b=2
+c=("Two numbers are equal")
+print(c)
+
+
+a=5
+b=9
+c=("One number is greater than other")
+print(c)
+
+
+a=4
+b=7
+c=("A number is less than or equal to another")
+print(c)
+
+a=int(input("Enter a:"))
+b=c*c
+print("Square root of the number",'b')
+
+
+a=int(input("Enter a:"))
+b=d*d*d
+print("Cube root of the number",'b')
+
+
+a=102938
+b=("Find the last two digit of the number")
+print(b)
+
+
+a=102s938
+b=("Remove the last two digit of the number")
+print(b)
