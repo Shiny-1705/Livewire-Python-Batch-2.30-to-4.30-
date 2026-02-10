15b90abcbdb8f822289c610f6f783b5014391457
vickyvinesh111-art <vickyvinesh111@gmail.com>
2026-02-10 02:54:13 -0800

    Add files via upload

---
+++ vinesh task 1.py
@@ -0,0 +1,99 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+======================= RESTART: C:/Users/Livewire/AppData/Local/Programs/Python/Python314/py.py =======================
+Enter a:5
+Enter b:2
+addition: 7
+subtraction: 3
+multipliplication: 10
+division 2.5
+modulus 1
+floor division 2
+exponentiation 25
+
+======================= RESTART: C:/Users/Livewire/AppData/Local/Programs/Python/Python314/py.py =======================
+Enter value:5
+Enter value:2
+area of rectangle: 5
+
+======================= RESTART: C:/Users/Livewire/AppData/Local/Programs/Python/Python314/py.py =======================
+Enter value:4
+area of squre: 16
+
+======================= RESTART: C:/Users/Livewire/AppData/Local/Programs/Python/Python314/py.py =======================
+Enter value:2
+area of circle: 3.14
+
+======================= RESTART: C:/Users/Livewire/AppData/Local/Programs/Python/Python314/py.py =======================
+Enter value:2
+Traceback (most recent call last):
+  File "C:/Users/Livewire/AppData/Local/Programs/Python/Python314/py.py", line 25, in <module>
+    v=3.14,D*D
+NameError: name 'D' is not defined
+
+======================= RESTART: C:/Users/Livewire/AppData/Local/Programs/Python/Python314/py.py =======================
+Enter value:2
+Traceback (most recent call last):
+  File "C:/Users/Livewire/AppData/Local/Programs/Python/Python314/py.py", line 25, in <module>
+    v=3.14,C*C
+NameError: name 'C' is not defined. Did you mean: 'c'?
+
+======================= RESTART: C:/Users/Livewire/AppData/Local/Programs/Python/Python314/py.py =======================
+Enter value:2
+area of circle: (3.14, 4)
+>>> 
+======================= RESTART: C:/Users/Livewire/AppData/Local/Programs/Python/Python314/py.py =======================
+ENTER THE MARK:18
+ENTER THE MARK:20
+ENTER THE MARK:22
+Traceback (most recent call last):
+  File "C:/Users/Livewire/AppData/Local/Programs/Python/Python314/py.py", line 32, in <module>
+    d=a+b+c/3
+NameError: name 'a' is not defined
+>>> 
+======================= RESTART: C:/Users/Livewire/AppData/Local/Programs/Python/Python314/py.py =======================
+ENTER THE MARK:18
+ENTER THE MARK:20
+ENTER THE MARK:22
+Traceback (most recent call last):
+  File "C:/Users/Livewire/AppData/Local/Programs/Python/Python314/py.py", line 33, in <module>
+    prit("average mark:",d)
+NameError: name 'prit' is not defined. Did you mean: 'print'?
+>>> 
+======================= RESTART: C:/Users/Livewire/AppData/Local/Programs/Python/Python314/py.py =======================
+ENTER THE MARK:18
+ENTER THE MARK:20
+ENTER THE MARK:22
+average mark: 45.333333333333336
+>>> 
+======================= RESTART: C:/Users/Livewire/AppData/Local/Programs/Python/Python314/py.py =======================
+Enter value:6
+enter value:5
+two numbers are equal: False
+>>> 
+======================= RESTART: C:/Users/Livewire/AppData/Local/Programs/Python/Python314/py.py =======================
+Enter value:5
+Enter value:3
+one number is greater then others: True
+>>> 
+======================= RESTART: C:/Users/Livewire/AppData/Local/Programs/Python/Python314/py.py =======================
+Enter value:5
+Enter value:6
+number is less than or equal to another: True
+>>> 
+======================= RESTART: C:/Users/Livewire/AppData/Local/Programs/Python/Python314/py.py =======================
+Enter value5
+square root of the number: 25
+
+======================= RESTART: C:/Users/Livewire/AppData/Local/Programs/Python/Python314/py.py =======================
+Enter value5
+cube root of the number: 125
+
+======================= RESTART: C:/Users/Livewire/AppData/Local/Programs/Python/Python314/py.py =======================
+Enter value:12345
+find the last 2 digit of number: 45
+
+======================= RESTART: C:/Users/Livewire/AppData/Local/Programs/Python/Python314/py.py =======================
+Enter value12345
+remove the last 2 digit number: 123
