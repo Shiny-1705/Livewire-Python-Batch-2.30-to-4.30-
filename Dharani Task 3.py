1f57dc01b7c82cca708c3ec0b9d2a5f43e81b0ac
Dharani 27 <dharanidharansuresh27@gmail.com>
2026-02-17 16:23:32 +0530

    Add files via upload

---
+++ Dharani Task 3.py
@@ -0,0 +1,63 @@
+'''for i in range(1,10,1):
+    print(i)'''
+
+
+'''a=int(input("Enter the value:"))
+for i in range(2,5):
+    print(a,'x',i,'=',a*i)'''
+
+
+
+'''for i in range(1,50):
+    if i%2==0:
+       print(i)'''
+
+
+'''n=int(input("Enter the value:"))
+f=1
+for i in range(1,n+1):
+  f=f*i
+  print("factorial:",f)'''
+
+
+
+'''for i in range(2,22):
+    if i%2==0:
+        print(i)'''
+
+
+
+'''for i in range(20,50):
+    if i%2==0:
+        print(i)'''
+
+
+
+'''for i in range(2,20):
+    if i%2!=0:
+        print(i)'''
+
+
+
+
+'''rows=int(input("Enter the row size for the pattern:"))
+for i in range(1,rows+1):
+    for j in range(1,rows+1):
+     if i==1 or j==4 or j==6:
+      print("*",end="")
+else:
+      print("",end="")
+      print()'''
+
+
+
+'''for i in range(65,91):
+    for j in range(65,i+1):
+        print(chr(j),end="")
+    print()'''
+
+
+
+
+
+
