fbe5167570490e449cc3bec5e6b6b79c8419ed64
jerryjerry81323-max <jerryjerry81323@gmail.com>
2026-03-02 15:29:29 +0530

    Add files via upload

---
+++ karthi task3.py
@@ -0,0 +1,19 @@
+'''for i in range(1,11):
+    print(i)
+
+
+a=int(input("Enter The Number:"))
+for i in range(1,6):
+    print(a,"X",i,"=",a*i)
+'''
+
+'''
+for i in range(2,51,2):
+    print(i)
+'''
+n=int(input("Enter The Number:"))
+f=1
+for i in range(1,n+1):
+    f=f*i
+print("factorial",f)
+    
