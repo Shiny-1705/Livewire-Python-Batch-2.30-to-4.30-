2b9c3a836d2645e01dcb03f7f43e3f09d8922e96
Dharani 27 <dharanidharansuresh27@gmail.com>
2026-03-04 18:43:16 +0530

    Add files via upload

---
+++ Dharani Task 5.py
@@ -0,0 +1,71 @@
+'''a=int(input("Enter a:"))
+b=0
+t=a
+while t>0:
+    d=t%10
+    b=(b*10)+d
+    t=t//10
+    print(b)
+    if a==b:
+       print("Palindrome")
+else:
+    print("Not palindrome")'''
+
+
+
+'''string=input("Enter your string:")
+print("Length:",len(string))'''
+
+
+
+'''def dharani(name,roll,dept):
+    print("Name:",name)
+    print("Roll:",roll)
+    print("Dept:",dept)
+dharani("Dharan","29","CS")'''
+
+
+'''def calculate_total(marks1,marks2,marks3):
+    print("Marks1:",marks1)
+    print("Marks2:",marks2)
+    print("Marks3:",marks3)
+calculate_total("90","95","97")'''
+
+
+
+'''def greet_user(name,message="Good Morning"):
+     print(message,name)
+greet_user("Dharani")'''
+
+
+'''def add_numbers(*args):
+    total=0
+    for n in args:
+        total+=n
+    print("Total=",total)
+add_numbers(5,7)'''
+
+
+
+'''def multiply_all(*args):
+    total=1
+    for n in args:
+        total*=n
+    print("Total=",total)
+multiply_all(5,7)'''
+
+
+'''numbers = [10, 324, 45, 90, 98]
+maximum_value = max(numbers)
+print(f"The maximum value is: {maximum_value}")'''
+
+
+
+'''numbers=[10,20,40,50,60]
+minimum_value=min(numbers)
+print(f"The minimum value is: {minimum_value}")'''
+
+
+
+ 
+
