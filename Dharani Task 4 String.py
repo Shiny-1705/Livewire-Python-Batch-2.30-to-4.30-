74991ac53df2e93f1528e47b11f223fbd4365500
Dharani 27 <dharanidharansuresh27@gmail.com>
2026-03-04 19:50:26 +0530

    Add files via upload

---
+++ Dharani Task 4 String.py
@@ -0,0 +1,56 @@
+'''a="Dharani"
+print(a.upper())'''
+
+
+'''b="Hii i am a Red Hat Hacker"
+print(b.lower())'''
+
+
+'''c="Hii i am a python developer"
+print(c.capitalize())'''
+
+
+'''d="age 25"
+e="Dharan"
+print(f"{e}, is a {d}")'''
+
+
+'''a="I am a person"
+print(a.index())'''
+
+
+'''a="string"
+print(a.endswith('ing'))'''
+
+
+'''a="Hacker\tEthicalhacker"
+print(a.expandtabs())'''
+
+
+'''a="Hello python"
+print(a.encode('utf-16'))'''
+
+
+'''h="87787"
+print(h.isdigit())'''
+
+
+'''n="27"
+print(n.isnumeric())'''
+
+
+'''m="123Jack"
+print(m.isalnum())'''
+
+
+'''x="I am a Java Developer"
+print(x.isascii())'''
+
+
+'''y="Dharani"
+print(y.isalpha())'''
+
+
+'''g="Python"
+h="Hii"
+print(f"{h}, this is {g}")'''
